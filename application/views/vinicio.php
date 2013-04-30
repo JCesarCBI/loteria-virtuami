195c5cb62550240d9827f211594f2b2c5f02c4ac
luisa.207310034@gmail.com <luisa.207310034@gmail.com@d24f31a6-42f3-10c3-1e4e-24255120a7fe>
2013-04-30 21:52:16 +0000

    30-04-2013 Luisa	Actualizaciones de archivos(vlogin a vinicio)

---
+++ application/views/vinicio.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+	<meta charset="utf-8">
+	
+	<title>Pruebas de funcionanmiento</title>
+	echo al header <br />
+	<link rel="stylesheet" href="<?=base_url(); ?>media/css/fondoLoguin.css">
+
+	</head>
+	<body>
+		echo cajon naye<br />
+		echo cajon Luisa
+			<div class="imagenes"></div>
+		
+	</body>
+</html>
