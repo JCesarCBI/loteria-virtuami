e0755d3a04c06a553cff9c0066d2a9db11fd14c8
luisa.207310034@gmail.com <luisa.207310034@gmail.com@d24f31a6-42f3-10c3-1e4e-24255120a7fe>
2013-05-04 00:30:12 +0000

    03-Mayo-2013 Luisa Vista con los primero inputs *Comenzando a probar el css de las vistas

--- application/views/vinicio.php
+++ application/views/vinicio.php
@@ -3,14 +3,26 @@
 	<meta charset="utf-8">
 	
 	<title>Pruebas de funcionanmiento</title>
+	<link rel="stylesheet" href="<?=base_url(); ?>media/css/foundation.css">
 	<link rel="stylesheet" href="<?=base_url(); ?>media/css/fondoLoguin.css">
 
 	</head>
 	<body>
-			<div class="centro">
-				<div class="cajonSuperior"></div>
-				<div class="mesa"></div>
-				<div class="cajonLateral"></div>
+		
+		<div class="nine columns">
+			<div class="cajonSuperior">
+				<br>
+				<br>
+				sdñksañdlsñldmsañld
+				sañdlksañldjñsal
+				lasjdñasljdñl
+			</div>
+			<div class="mesa"></div>
+			<div class="cajonLateral">
+                <input autofocus type="text" id="usuario_nombreUsr" name="usuario_nombreUsr" required placeholder="usuario"/>
+                <input autofocus type="text" id="usuario_correo" name="usuario_correo" required placeholder="e-mail"/>
+                <input autofocus type="text" id="usuario_contrasena" name="usuario_contrasena" required	placeholder="contraseña" />
+			</div>
 						<!--img src="<?=base_url();?>media/img/mesa.png"  class="mesa2"/>
 						<img src="<?=base_url();?>media/img/cajon_reg.png" class="cajon2"/-->
 			
