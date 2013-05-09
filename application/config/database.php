940ef16f98f6a33e03c14a42d51897f57c97f084
JCesarCBI <JCesarCBI@d24f31a6-42f3-10c3-1e4e-24255120a7fe>
2013-05-09 18:57:08 +0000

    09/05/13 Cesar Padilla Modificación del archivo "mRegistro.php"

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'admin';
-$db['default']['database'] = 'admin';
+$db['default']['password'] = '';
+$db['default']['database'] = 'loteriadb';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
