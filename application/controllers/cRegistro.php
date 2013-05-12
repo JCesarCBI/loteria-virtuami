351f7bf9be214acd0d8bbb0056b43ed387ecef46
guillermotorreslopez <guillermotorreslopez@d24f31a6-42f3-10c3-1e4e-24255120a7fe>
2013-05-12 03:23:08 +0000

    11/05/2013 10:22 Agrego Validacion de los datos de registro para agregarlos al modelo

--- application/controllers/cRegistro.php
+++ application/controllers/cRegistro.php
@@ -11,7 +11,43 @@
 	}
 	
 	
-	
+	public function validarRegistro(){
+        // Reglas de validacion
+        	
+            $this->form_validation->set_rules('usuario_nombreUsr','','required');
+            $this->form_validation->set_rules('usuario_correo','','required|trim|valid_email');
+			$this->form_validation->set_rules('usuario_contrasena','','required|trim|min_length[6]');
+            $this->form_validation->set_rules('usuario_nombre','required|trim');		
+			$this->form_validation->set_rules('usuario_aPaterno','required|trim');
+			$this->form_validation->set_rules('usuario_aMaterno','required|trim');
+			
+            
+           
+            $this->form_validation->set_message('required','El Campo %s Es Obligatorio');
+            $this->form_validation->set_message('valid_email','Ingrese un %s Válido');
+            $this->form_validation->set_message('matches','El Campo %s no es igual que el campos %s');
+            $this->form_validation->set_message('min_length','El Campo %s debe tener como minimo 6 caracteres');
+           
+            if($this->form_validation->run() != FALSE)
+            {
+               
+                $data = array('mensaje'=>'El usuario se registro correctamente');
+                $datosUsuario= array(
+                            'usuario'=>$this->input->post('usuario_nombreUsr',TRUE),
+                            'correo'=>$this->input->post('usuario_correo',TRUE),
+                            'contrasena'=>$this->input->post('usuario_contrasena',TRUE),
+                            'sexo'=>$this->input->post('usuario_sexo',TRUE),
+                            'nombre'=>$this->input->post('usuario_nombre',TRUE),
+                            'aPaterno'=>$this->input->post('usuario_aPaterno',TRUE),
+                            'aMaterno'=>$this->input->post('usuario_aMaterno',TRUE));
+				//Regresar el arreglo con los datos del usuario para que el modelo los agregue a la base de datos e iniciar sesión	
+            }
+            else
+            {
+          
+                //Regresar al registro
+            }
+        }
 	//Función AJAX que verifica si el usuario existe o no existe en la BD
 	function usuario(){
 		$term = $this->input->post('usuario',TRUE); //Recibo variable "usuario" a través de AJAX. Archivo media/js/inicio.js. Línea 90
@@ -32,7 +68,8 @@
 	
 	function correo(){
 		$term = $this->input->post('correo',TRUE); //Recibo variable "correo" a través de AJAX. Archivo media/js/inicio.js. Línea 119
-		$valor=$valor= $this->mregistro->getExisteCorreo($term);//La función 'getExisteCorreo' regresa true si el ususario existe y false en caso contrario.
+		
+		$valor= $this->mregistro->getExisteCorreo($term);//La función 'getExisteCorreo' regresa true si el correo existe y false en caso contrario.
 		//Envia la respuesta a la vista si el correo existe o no en la BD
 		if( $valor){  
 			echo json_encode(1);	
@@ -50,8 +87,11 @@
 			echo json_encode(0);
 		}
 	}
-
 	
+	
+		
 	}
+	
+	
 
 	
