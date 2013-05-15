a37bcc88da0143e8c8396799eb7a552b00a1368d
guillermotorreslopez <guillermotorreslopez@d24f31a6-42f3-10c3-1e4e-24255120a7fe>
2013-05-15 21:37:35 +0000

    15/05/2013 GT última versióndespués del error del repositorio...

--- application/controllers/cRegistro.php
+++ application/controllers/cRegistro.php
@@ -1,4 +1,5 @@
-<?php
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
     class CRegistro extends CI_Controller {
     public function __construct(){
         parent::__construct();
@@ -6,21 +7,13 @@
         $this->load->library('form_validation');
         $this->load->model('usuario/mregistro');
 		
-    }
-    public function index(){
-        // Cargar vista de formulario
-      //  $this->load->view('vPruebasGuillermo');
-    }
-    
-    /*public function registro()
-    {
-        $this->load->view('vinicio2');
-    }
-    */
+		
+	}
 	
-    public function validarRegistro(){
+	//Valida el registro y agrega los datos a la base de datos 
+	public function validarRegistro(){
         // Reglas de validacion
-        
+        	
             $this->form_validation->set_rules('usuario_nombreUsr','','required');
             $this->form_validation->set_rules('usuario_correo','','required|trim|valid_email');
 			$this->form_validation->set_rules('usuario_contrasena','','required|trim|min_length[6]');
@@ -29,8 +22,7 @@
 			$this->form_validation->set_rules('usuario_aMaterno','required|trim');
 			
             
-           // $this->form_validation->set_rules('','','required|trim|matches[pass]');
-            
+           
             $this->form_validation->set_message('required','El Campo %s Es Obligatorio');
             $this->form_validation->set_message('valid_email','Ingrese un %s Válido');
             $this->form_validation->set_message('matches','El Campo %s no es igual que el campos %s');
@@ -47,45 +39,58 @@
                             'sexo'=>$this->input->post('usuario_sexo',TRUE),
                             'nombre'=>$this->input->post('usuario_nombre',TRUE),
                             'aPaterno'=>$this->input->post('usuario_aPaterno',TRUE),
-                            'aMaterno'=>$this->input->post('usuario_aMaterno',TRUE),
-							'edad'=>$this->input->post('usuario_edad',TRUE),
-							'comunidad'=>$this->input->post('usuario_comunidadUniversitaria',TRUE),
-							'division'=>$this->input->post('usuario_comunidadUniversitaria',TRUE));			
+                            'aMaterno'=>$this->input->post('usuario_aMaterno',TRUE));
+				//Regresar el arreglo con los datos del usuario para que el modelo los agregue a la base de datos e iniciar sesión	
             }
             else
             {
-                //$this->load->view('vinicio2');
+          
+                //Regresar al registro
             }
         }
-        
+	//Función AJAX que verifica si el usuario existe o no existe en la BD
+	function usuario(){
+		$term = $this->input->post('usuario',TRUE); //Recibo variable "usuario" a través de AJAX. Archivo media/js/inicio.js. Línea 90
 		
-	
-		function existeUsuario(){
-       	
-        $term = $this->input->post('usuario',TRUE); //Recibo variable "usuario" a través de AJAX. Archivo media/js/inicio.js. Línea 90
-		 $valor= $this->mregistro->getExisteUsuario($term);
+		$valor= $this->mregistro->getExisteUsuario($term);//La función 'getExisteUsuario' regresa true si el ususario existe y false en caso contrario.
+		
 		//Envia respuesta a la vista si el usuario existe o no en la BD
-		if($valor){  
+		if( $valor){
+			echo "El usuario existe";  
 			echo json_encode(1);	
 		}else{
+			echo "El usuario no existe";
 			echo json_encode(0);
 		}		
-    	}
-   
-    	function existeCorreo(){
-    		$term = $this->input->post('correo',TRUE); //Recibo variable "correo" a través de AJAX. Archivo media/js/inicio.js. Línea 119
-			$valor = $this->mregistro->getExisteCorreo($term);
+	}
+	
+	
+	
+	function correo(){
+		$term = $this->input->post('correo',TRUE); //Recibo variable "correo" a través de AJAX. Archivo media/js/inicio.js. Línea 119
+		
+		$valor= $this->mregistro->getExisteCorreo($term);//La función 'getExisteCorreo' regresa true si el correo existe y false en caso contrario.
 		//Envia la respuesta a la vista si el correo existe o no en la BD
 		if( $valor){  
 			echo json_encode(1);	
 		}else{
 			echo json_encode(0);
-		}					}
+		}					
+	}
 	
-		function datosComUniv(){
-			$comUniv = $this->mregistro->getTipoUsuario();
-			return $comUniv;
+	function vacio($input){
+		$term = $this->input->post($input,TRUE);
+	
+		if($term == ""){
+			echo json_encode(1);	
+		}else{
+			echo json_encode(0);
 		}
-
-}
+	}
+	
+	
+		
+	}
+	
+	
 ?>
