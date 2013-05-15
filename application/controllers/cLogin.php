1c5694bb393dc024aae19f91b48c55316d38e370
jezrelmx1304 <jezrelmx1304@d24f31a6-42f3-10c3-1e4e-24255120a7fe>
2013-05-15 00:30:06 +0000

    Pruebas de enviar email

--- application/controllers/cLogin.php
+++ application/controllers/cLogin.php
@@ -6,6 +6,7 @@ class CLogin extends CI_Controller {
         $this->load->helper(array('html', 'url', 'form'));
 		$this->load->helper('form');
 		$this->load->library('form_validation');
+		//$this->load->library('email');
 		$this->load->model('usuario/mlogin');
 	}
 	
@@ -45,29 +46,47 @@ class CLogin extends CI_Controller {
         	$Usuario = $this->mlogin->getContrasena($correo);
 			//echo print_r($Usuario);
 			if($Usuario){
+				// Set SMTP Configuration
 				$config = Array(
-					'useragent'      => 'CodeIgniter',
-					'mailpath'     => '/usr/sbin/sendmail',// or "/usr/sbin/sendmail"
-					'protocol'     => 'mail',
-					'smtp_host'     => 'localhost',
-					'smtp_user'     => '',
-					'smtp_pass'      => '',
-					'smtp_port'  =>  '25', 
-					);
-				$this->load->library('email', $config);				
-				$this->email->from('tu_direccion@tu_sitio.com', 'LOTERIA UAMI');
-				$this->email->to("jcesarcbi@gmail.com");
-				//$this->email->to($correo);
-				$this->email->subject('Te enviamos tu contraseña de LOTERIA UAMI');
-				$this->email->message('Tu usuario es hola');
-				//$this->email->message('Tu usuario es '.$Usuario[0]->nombreUsr.'y tu contraseña es '.$Usuario[0]->contrasena);
-				$this->email->send();
-				echo $this->email->print_debugger();
-				echo "CORREO ENVIADO";
-			}else{
-				echo "No tienes cuenta en Loteria UAM, registrate...";
-				$this->load->view('vinicio2');
+			    'protocol' => 'smtp',
+			    'smtp_host' => 'ssl://smtp.googlemail.com',
+			    'smtp_port' => 465,
+			    'smtp_user' => 'ludico@virtuami.izt.uam.mx',
+			    'smtp_pass' => '7Ud1C0u@m',
+			    'mailtype'  => 'html', 
+			    'charset'   => 'iso-8859-1'
+				);
+				$config['protocol']    = 'smtp';
+		        $config['smtp_host']    = 'ssl://smtp.googlemail.com';
+		        $config['smtp_port']    = '465';
+		        $config['smtp_timeout'] = '7';
+		        $config['smtp_user']    = 'ludico@virtuami.izt.uam.mx';
+		        $config['smtp_pass']    = '7Ud1C0u@m';
+		        $config['charset']    = 'iso-8859-1';
+		        $config['newline']    = "\r\n";
+		        $config['mailtype'] = 'html'; // or html
+		        $config['validation'] = TRUE; // bool whether to validate email or not      
+		
+		        $this->load->library('email', $config);
+		
+		        $this->email->from('ludico@virtuami.izt.uam.mx', 'myname');
+		        $this->email->to('jezrelmx1304@gmail.com'); 
+		
+		        $this->email->subject('Email Test');
+		        $this->email->message('Testing the email class.');  
+		
+		        $this->email->send();
+		
+		        echo $this->email->print_debugger();
+		
+		        $this->load->view('email_view');
+				
+							
+				}else{
+					echo "No tienes cuenta en Loteria UAM, registrate...";
+					
 			}        	
         }
+		$this->load->view('vinicio2');
 	}
 }
