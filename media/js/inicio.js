8a02a151e1ca980699e94b097bc357a21f46b668
anjudark89 <anjudark89@d24f31a6-42f3-10c3-1e4e-24255120a7fe>
2013-05-17 05:40:08 +0000

    13 mayo 2013 Nallely Se agregaron eventos para la validación del registro

--- media/js/inicio.js
+++ media/js/inicio.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	
 	//prepara campos para mostrar la primera parte del formulario
-	$('.cajonLateral-abierto, .cajonSuperior-abierto, .error, .ok, .recuperarContrasena').hide();
+	$('.cajonLateral-abierto, .cajonSuperior-abierto, .ok, .recuperarContrasena, .error_validation').hide();
 	$('#usuario_nombreUsr, #usuario_correo, #usuario_contrasena, #usuario_nombre, #usuario_aPaterno, #usuario_aMaterno,#usuario_edad, #usuario_comunidadUniversitaria, #usuario_division').val("");
 	$("#sig1, #sig2, #enviar").attr("disabled","disabled")
 
@@ -81,6 +81,7 @@ $(document).ready(function() {
 
 		if(usr == ""){
 			$('#usuario_nombreUsr').removeClass("correcto incorrecto").addClass("vacio")
+			$('#usrError').hide();
 			$('#usr_ok').val(0)
 		}
      	
@@ -93,10 +94,12 @@ $(document).ready(function() {
 				if(existe == 1){
 					$('#usuario_nombreUsr').removeClass("vacio correcto").addClass("incorrecto")
 					$('#usr_ok').val(0)
+					$('#usrError').show();
 				}else{//Si el usuario no existe 
 					if(usr != ""){ //se verifica primero que el campo no esté vacío para añadir la clase correcto
 						$('#usuario_nombreUsr').removeClass("vacio incorrecto").addClass("correcto")
 						$('#usr_ok').val(1)
+						$('#usrError').hide();
 					}else{
 						$('#usr_ok').val(0)
 					}
@@ -115,28 +118,65 @@ $(document).ready(function() {
 		})
 
 		if(correo == ""){
-			// alert("correo vaío if")
 			$('#email_ok').val(0)
 			$('#usuario_correo').removeClass("correcto incorrecto").addClass("vacio")
-		}else{   		
+			$('#emailDominioError').hide()
+		}else{	   		
+			//Se comprueba el dominio
 			$.ajax({
-				url: base+'index.php/cpruebasNaye/correo',
+				url: base+'index.php/cpruebasNaye/dominio',
 				data: { correo: $('#usuario_correo').val() },
 				dataType: "json",
 				type: "POST",
-				success:function(existe){ //Si el usuario existe, mostrará la clase incorrecto
-					if(existe == 1){
+				success:function(correcto){ //Si el dominio no es correcto, mostrará la clase incorrecto y el mensaje de alerta
+					if(correcto == 0){
 						$('#usuario_correo').removeClass("vacio correcto").addClass("incorrecto")
 						$('#email_ok').val(0)
+						$('#emailExisteError').hide()
+						$('#emailDominioError').show()
+						$('#email_ok').val(0)
 					}else{
-						//Si el usuario no existe, se verifica primero que el campo no esté vacío para añadir la clase correcto
-						if(correo != ""){
-							$('#usuario_correo').removeClass("vacio incorrecto").addClass("correcto")
-							$('#email_ok').val(1)
-						}else{
-							alert("correo vacío")
-							$('#email_ok').val(0)
-						}
+						//Si el deominio es correcto, comprobará que el correo no exista
+						$.ajax({
+							url:base+'index.php/cpruebasNaye/correo',
+							data:{ correo: $('#usuario_correo').val() },
+							dataType: "json",
+							type: "POST",							
+							success:function(existe){
+								if(existe == 1){
+									$('#usuario_correo').removeClass("vacio correcto").addClass("incorrecto")
+									$('#emailExisteError').show()
+									$('#emailDominioError').hide()
+									$('#email_ok').val(0)
+									
+									var pass = $('#usuario_contrasena').val();
+									var usr_ok = $('#usr_ok').val();
+									var email_ok = $('#email_ok').val();
+					        		if(usr_ok == '1' && email_ok == '1' && pass != ''){
+										$("#sig1").removeAttr("disabled")
+									}else{
+										$("#sig1").attr("disabled","disabled")
+									}
+								}else{ 	//si el correo no existe, verifica que el campo no esté vacío para añadir la clase correcto
+									if(correo != ""){
+										$('#usuario_correo').removeClass("vacio incorrecto").addClass("correcto")
+										$('#email_ok').val(1)
+										$('#emailExisteError, #emailDominioError').hide()
+										var pass = $('#usuario_contrasena').val();
+										var usr_ok = $('#usr_ok').val();
+										var email_ok = $('#email_ok').val();
+						        		if(usr_ok == '1' && email_ok == '1' && pass != ''){
+											$("#sig1").removeAttr("disabled")
+										}else{
+											$("#sig1").attr("disabled","disabled")
+										}
+									}else{
+										$('#email_ok').val(0)
+										$('#emailExisteError, #emailDominioError').hide()
+									}									
+								}
+							}
+						})
 	        		}
 					//Desbloqueo del primer botón siguiente sólo si los 3 campos están llenos correctamente
 					//Y si el usuario ni el correo existen en la BD
