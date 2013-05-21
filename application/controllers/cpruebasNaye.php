dbda74ad239b4c6ab1ec789a1a82af61e0ffabbd
anjudark89 <anjudark89@d24f31a6-42f3-10c3-1e4e-24255120a7fe>
2013-05-21 18:24:35 +0000

    21 mayo Nallely Agregué función para recuperar contraseña mediante AJAX

--- application/controllers/cpruebasNaye.php
+++ application/controllers/cpruebasNaye.php
@@ -10,6 +10,8 @@ class cpruebasNaye extends CI_Controller {
 		$this->load->helper('form'); //Contiene funciones que ayuda a trabajar con formularios en html 
 		$this->load->model('usuario/mregistro');
 		$this->load->library('form_validation');
+		$this->load->model('usuario/mlogin');
+		
 	  }
 	
 	public function index()	{
@@ -53,8 +55,7 @@ class cpruebasNaye extends CI_Controller {
 			$term = $this->input->post('correo',TRUE); //Recibo variable "correo" a través de AJAX. Archivo media/js/inicio.js. Línea 119
 			$valor= $this->mregistro->getExisteCorreo($term);//La función 'getExisteCorreo' regresa true si el correo existe y false en caso contrario.
 			$correo_xanum = preg_match('/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@]xanum[.]uam[.]mx$/', $term);
-			$correo_titlani = preg_match('/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@]titlani[.]uam[.]mx$/', $term);
-			if( $valor || ($correo_xanum == FALSE && $correo_titlani==FALSE)){
+			if( $valor){
 				//Correo existe   
 				echo json_encode(1);	
 			}else{
@@ -67,22 +68,41 @@ class cpruebasNaye extends CI_Controller {
 			$term = $this->input->post('correo',TRUE); //Recibo variable "correo" a través de AJAX. Archivo media/js/inicio.js. Línea 119
 			$correo_xanum = preg_match('/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@]xanum[.]uam[.]mx$/', $term);
 			$correo_titlani = preg_match('/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@]titlani[.]uam[.]mx$/', $term);
+			$correo_docencia = preg_match('/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@]docencia[.]uam[.]mx$/', $term);
+			$correo_titlani = preg_match('/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@]titlani[.]uam[.]mx$/', $term);
+			
 			//Comprobando si el dominio es correcto
-			if($correo_xanum == FALSE && $correo_titlani==FALSE){
+			if($correo_xanum == FALSE && $correo_titlani==FALSE && $correo_docencia==FALSE){
 				echo json_encode(0);	
 			}else{
 				echo json_encode(1);
 			}					
 			
 		}
-		function vacio($input){
-			$term = $this->input->post($input,TRUE);
 		
-			if($term == ""){
+		//Esta función estará definida por el ajax para enviar el correo para recuperar la contraseña
+		public function recuperarContrasena(){
+			$correo = $this->input->post('correo',TRUE);
+			$Usuario = $this->mlogin->getContrasena($correo);
+			if($Usuario){ //Enviará el correo. No imprimir ningún echo más que el json_encode
+				$config['protocol']    = 'smtp';
+		        $config['smtp_host']    = 'ssl://smtp.googlemail.com';
+		        $config['smtp_port']    = '465';
+		        $config['smtp_timeout'] = '7';
+		        $config['smtp_user']    = 'ludico@virtuami.izt.uam.mx';
+		        $config['smtp_pass']    = '7Ud1C0u@m';
+		        $config['charset']    = 'iso-8859-1';
+		        $config['newline']    = "\r\n";
+		        $config['mailtype'] = 'html'; // or html
+		        $config['validation'] = TRUE;    
+		        $this->load->library('email', $config);
+		        $this->email->from('ludico@virtuami.izt.uam.mx', 'myname');
+		        $this->email->to($correo); 
+		        $this->email->subject('Email Test');
+		        $this->email->message('Tu usuario es '.$Usuario[0]->nombreUsr. 'y tu contraseña es'.$Usuario[0]->contrasena);  	
 				echo json_encode(1);	
-			}else{
+			}else{ //El correo no existe
 				echo json_encode(0);
-			}
-		}
-
-}
+			}        	
+        }
+	}
