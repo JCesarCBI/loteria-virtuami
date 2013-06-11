4722aba7a1f6dc311425b3e71882cd3e09e4400a
anjudark89 <anjudark89@d24f31a6-42f3-10c3-1e4e-24255120a7fe>
2013-06-11 14:37:50 +0000

    11-MAYO-2013 Nallely Flores Se creó la nueva vista vlecciones.php

---
+++ application/views/vlecciones.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="<?=base_url(); ?>media/css/foundation.css">
+		<link rel="stylesheet" href="<?=base_url(); ?>media/css/general.css">
+		<link rel="stylesheet" href="<?=base_url(); ?>media/css/lecciones.css">
+		<script src="<?= base_url()?>media/js/jquery-1.9.1.min.js"></script>
+		<script> base = "<?= base_url() ?>"</script>
+		<script src="<?= base_url()?>media/js/lecciones.js"></script>
+	</head>
+
+	<body>
+		<title>Inicio</title>
+		
+		<div class="twelve header">
+		
+		<div class="row">
+			<div class="two columns"></div>
+			<div class="seven columns mueble">
+				<img src="<?= base_url()?>media/img/mueble.png" />
+			</div>
+			<div class="three columns mesa"></div>
+		</div>
+	</body>
+</html>
