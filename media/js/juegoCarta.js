705d9e1f4ac01a8644aca2527dd5131c87b031e3
luisa.207310034@gmail.com <luisa.207310034@gmail.com@d24f31a6-42f3-10c3-1e4e-24255120a7fe>
2013-06-14 22:26:59 +0000

    luisa 14/Junio/2013 Actualizaciones del juego 2

--- media/js/juegoCarta.js
+++ media/js/juegoCarta.js
@@ -1,8 +1,43 @@
-//Variables globales
-var cantidad=54;
-var ultimaCarta;
-var cadenaId;
-var auxiliar=0;
+function obternerId(indice){
+	
+		//Obtengo todos los id de las cartas dey los guardo en una arreglo
+		 var cartasArreglo=document.getElementById('IdCartaReversa').value;
+		 cartasId=cartasArreglo.split("*");
+		 return	cartasId[indice]; 
+}
+
+function obternerIdPlantilla(indice){
+	
+		//Obtengo todos los id de las cartas de la plantilla y los guardo en una arreglo
+		 var cartasArreglo=document.getElementById('IdMazoReversa').value;
+		 cartasId=cartasArreglo.split("*");
+		 return	cartasId[indice]; 
+}
+
+//Esta funcion se encarga de escribir las rimas y reproducir el audio de la carta que se muestr
+function audioRima(indice){
+	
+		document.getElementById('cartaVisible').value=indice;
+		 idCarta=obternerId(indice)
+		 //Cuando se llegue al ultimo elemento del arreglo se regresará una elemento vacio.
+		if (idCarta != "") {
+			
+			//llamo a la función ajax_escribeRima() y le mando su correspondiente Id)
+			ajax_escribeRima(cartasId[indice]);			
+			$('embed').remove();
+			//$('#audio').html('<embed src="'+base+'media/audio/wa_wa_waaa.mp3" autostart="true" hidden="true" loop="false" />');
+				
+			return 0; 
+		} else{
+		
+			return 1;
+		
+		};
+		 
+	
+}
+
+//Esta funcion se encarga de escribir la rima del elemento
 function ajax_escribeRima(id){
 		
 		var url = base+'index.php/cpruebasLuisa/descripcion/'+id;
@@ -10,6 +45,7 @@ function ajax_escribeRima(id){
 			$.ajax({
 	    
 	        url: url,
+  			async: false,
 	               
 	        success: function(data){ 
 	               
@@ -26,89 +62,199 @@ function ajax_escribeRima(id){
 	
 }
 
-function cambiarCarta() {
-	 
-	//Obtengo el id de la carta que volteare por medio del value --(el value tiene el formato baraja-IdQueCambiar)--   
-	var carta=document.getElementById('IdCartaReversa').value;	
-	$(cartaReversaClick).removeAttr("onclick");
-	//Obtendre un arreglo con 2 cosas	
-	var n=carta.split("-");
-	
-	if(n[1]==1){
-		cadenaId=n[0].split("*");
-		document.getElementById('IdCartaReversa').value="baraja-"+id+"-2";
-		$("#baraja-"+cadenaId[0]).removeClass("Escondido");
+//Esta funcion se encarga de traerme la respuesta correcta dependiendo del Id
+function ajax_validarRespuesta(id, respuesta){
 		
+		var respuestaCorrecta=1;
+		var url = base+'index.php/cpruebasLuisa/respuestaCorrecta/'+id;
+	
+			$.ajax({
+	    
+	        url: url,
+  			async: false,
+	               
+	        success: function(data){
+	        	 
+			    if(data== respuesta){
+					
+						respuestaCorrecta=0; 
+				} 
+	            
+	        },
+	        
+	        error: function(){
+	           alert("Error al traer la respuesta");
+	        }
+	    
+	    });
+	    
+	    return respuestaCorrecta;
+	
+}
+
+
+function cambiaCarta(numCarta) {
+	//elimino el input para que clickeen la nueva carta
+	$('#respuestaInput').html("");
+	$('#cartaReversaClick').removeAttr("onclick");
+	//En esta función regreso 0 si la carta fue encontrada, en caso contrario regreso 0
+	var continuar= audioRima(numCarta);
+  	
+  	//si el numero de la carta es 0, quiere decir que es la primeravez que volteare una carta
+  	if(numCarta==0){
+		$("#baraja-"+numCarta).removeClass("Escondido");
+		
+	} else{
+		
+		//Quito la clase Escondido a la carta que me indiquen el id
+		var cartaAnt=numCarta-1;
+		$('#baraja-'+cartaAnt).addClass("Escondido");
+		
+		//Escondo la carta que estaba antes a la vista
+		$("#baraja-"+numCarta).removeClass("Escondido");
+						
+	}
+	
+	
+	if (continuar== 0) {
+			//Como se encontro la carta incremento numCarta para ir por la siguiente carta
+			numCarta++;
+			setTimeout("cambiaCarta("+numCarta+")",10000);
+		 
+		} else{
+			//Si no se encontro la cart solo escondo la última que se mostró
+			numCarta--;
+			$('#baraja-'+numCarta).addClass("Escondido");			
+		};
+			
+
+}
+
+function presionaEnter(evt, op) {
+
+	indice=document.getElementById('cartaVisible').value;
+	$('#baraja-'+indice).removeAttr("onclick");
+	 var charCode = (evt.which) ? evt.which : event.keyCode
+	 
+	 if (charCode == 13) {
+	//Presiond enter obtengo la respuesta escrita
+	 		 var respuesta=document.getElementById('respuestaBaraja').value;
+	 		 id=obternerId(indice);
+	 		 
+			var vOf = ajax_validarRespuesta(id, respuesta)
+			if (vOf==1) {
+				//rompo cadenas
+				//Reescribo el input e indico que la siguiente vez que presione enter sera la ultima
+				$('#respuestaInput').html('<input type="text" name="respuestaBaraja" id="respuestaBaraja" value="" onkeydown="javascript:return presionaEnter(event, 2)" >');
+				
+				//pongo el curso en el input
+				$('#respuestaBaraja').focus();
+			
+				if (op ==2){
+					$('#respuestaInput').html(' ');
+					alert("perdiste tu segunda oportunidad en la baraja, TIENES UN ERROR");
+				}
+			}
+			else{
+				
+				//Incremento multiplicador
+				
+			}
+		}
+
+	
+}
+
+function clickBaraja(id) {
+	//Escribo el input en el cual se escribirá la respuesta
+    $('#respuestaInput').html('<input type="text" name="respuestaBaraja" id="respuestaBaraja" value="" onkeydown="javascript:return presionaEnter(event, 1)">');
+	//pongo el curso en el input
+    $('#respuestaBaraja').focus();
+	
+}
+
+
+function presionaEnterPlantilla(evt, op) {
+
+	indice=document.getElementById('cartaClickPlantilla').value;
+	$('#plantilla-'+indice).removeAttr("onclick");
+	
+	 var charCode = (evt.which) ? evt.which : event.keyCode
+	 
+	 
+	 if (charCode == 13) {
+	//Presiond enter obtengo la respuesta escrita
+	 		 var respuesta=document.getElementById('respuestaBaraja').value;
+	 		 id=obternerIdPlantilla(indice);
+	 		 
+	 		 
+			var vOf = ajax_validarRespuesta(id, respuesta)
+	 		 alert(respuesta+ id+ vOf);
+			if (vOf==1) {
+				//rompo cadenas
+				//Reescribo el input e indico que la siguiente vez que presione enter sera la ultima
+				$('#respuestaInput').html('<input type="text" name="respuestaBaraja" id="respuestaBaraja" value="" onkeydown="javascript:return presionaEnterPlantilla(event, 2)" >');
+				
+				//pongo el curso en el input
+				$('#respuestaBaraja').focus();
+			
+				if (op ==2){
+					$('#respuestaInput').html(' ');
+					alert("perdiste tu segunda oportunidad en la plantilla, TIENES UN ERROR");
+				}
+			}
+			else{
+				
+				//Incremento score
+				
+			}
+
+	
+		}
+}
+
+function validaPlantillaBaraja(indice){
+	document.getElementById('cartaClickPlantilla').value=indice;
+	indiceBaraja=document.getElementById('cartaVisible').value;
+	idCartaBaraja=obternerId(indiceBaraja);
+	idCartaPlantilla=obternerIdPlantilla(indice);
+	if(idCartaPlantilla==idCartaBaraja){
+		return 0;
 			
 	} else{
-			
-		//Quito la clase Escondido a la carta que me indiquen el id
-		$('#baraja-'+cadenaId[0]).addClass("Escondido");
 		
-		//Escondo la carta que estaba antes a la vists
-		$("#baraja-"+cadenaId[1]).removeClass("Escondido");
-		
-		ultimaCarta=cadenaId[1];
-		
-		primero=cadenaId.shift();
-		
+		return 1
 	}
 	
-	var id=parseInt(cadenaId[0]);
-	
-		document.getElementById('cartaVisible').value=id;
-		ajax_escribeRima(id);
-	
-	
-		//Aumento el id para dejar preparado el value con la siguiente carta
-		//Cambia la carta cada segundo
-		if (id<cantidad) {
-			
-			if (auxiliar == 0) {
-			var ahoraSonLas=setTimeout("cambiarCarta()",3000);
-			};	
-			
-		} else
-			$('#baraja-'+ultimaCarta).addClass("Escondido");
 	
 }
 
-
-function compararCarta(id) {
-   
-	var carta=document.getElementById('IdMazoReversa').value;
-	 
-	document.getElementById('cartaVisible').value
+function clickPlantilla(indice) {
 	
-	var	arreglo=carta.split("*");
+	document.getElementById('cartaClickPlantilla').value=indice;
+	//Escribo el input en el cual se escribirá la respuesta
+    $('#respuestaInput').html('<input type="text" name="respuestaBaraja" id="respuestaBaraja" value="" onkeydown="javascript:return presionaEnterPlantilla(event, 1)">');
+	//pongo el curso en el input
+    $('#respuestaBaraja').focus();
 		
-	var busqueda=id.toString();
-		
-		arreglo.pop()
-		
-	if(arreglo.indexOf(busqueda)!= '-1'){
-			
-		alert("la carta esta en el mazo coño!!!"+ busqueda)
-		
-	}else{
-		
-		alert("la carta no esta en el mazo "+ busqueda)
-	}
+}
+
+
+function multiplicadores() {
 	
 }
 
-function compararCartaMazo(id) {
+function errores() {
 	
-	auxiliar =1;
-	var cartaVisible=document.getElementById('cartaVisible').value;
-		
-	alert(id + "  " +cartaVisible);
-	if(cartaVisible==id){
-		clearTimeout(ahoraSonLas);
-		alert("Deberia detenerse");
-		
-	}
-	else {
-	}
+}
+
+function puntos() {
+	
+}
+
+function comodines() {
+	
+}
+function rompeCadenas() {
 	
 }
