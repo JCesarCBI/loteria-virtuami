2c346f0a04de3f8cf6e3d16eeb0c6aa83c3e341d
luisa.207310034@gmail.com <luisa.207310034@gmail.com@d24f31a6-42f3-10c3-1e4e-24255120a7fe>
2013-07-13 21:51:17 +0000

    13-Julio-2013 Luisa vista modalidad con botones

--- application/views/vModalidad.php
+++ application/views/vModalidad.php
@@ -5,26 +5,36 @@
 		<title>Principal</title>
 		<script src="<?=base_url(); ?>media/js/jquery-1.9.1.min.js" type="text/javascript" charset="utf-8"></script>
 		<script>var base = "<?=base_url(); ?>"</script> 
+		<script src="<?=base_url(); ?>media/js/modalidadJuego.js" type="text/javascript" charset="utf-8"></script>
 		<link rel="stylesheet" href="<?=base_url(); ?>media/css/foundation.css">
 
 	</head>	
 	
 	<body>
+		<input type="hidden" id="vPartida" value="0" />
+		<input type="hidden" id="vNivel" value="0" />
+		<input type="hidden" id="vModalidad" value="0" />
+		
 		<h2>Principal</h2>
 		
 		<center> SELECCIONA UNO	 <br />
-		<a href="#" class="button round">PARTIDA COMPLETA</a>	
-		<a href="#" class="button round">PARTIDA RÁPIDA</a> <br /> <br />
+		<a href="#" id="pCompleta" class="button round">PARTIDA COMPLETA</a>	
+		<a href="#" id="pRapida" class="button round">PARTIDA RÁPIDA</a> <br /> <br />
 		SELECCIONA UN NIVEL	 <br />
-		<a href="#" class="button round">Básico</a>	
-		<a href="#" class="button round">Intermedio</a>
-		<a href="#" class="button round">Avanzado</a>
-		<a href="#" class="button round">Experto</a> <br /> <br />
+		<a href="#" id="nBasico" class="button round">Básico</a>	
+		<a href="#" id="nIntermedio" class="button round">Intermedio</a>
+		<a href="#" id="nAvanzado" class="button round">Avanzado</a>
+		<a href="#" id="nExperto" class="button round">Experto</a> <br /> <br />
 		SELECCIONA UNA MODALIDAD	 <br />
-		<a href="#" class="button round">Juego Libre</a>	
-		<a href="#" class="button round">Diminutivo</a>
-		<a href="#" class="button round">Derivados y adjetivos</a>	
-		<a href="#" class="button round">Sinonimos</a>
-		 </center>	
+		<a href="#" id="mJL" class="button round">Juego Libre</a>	
+		<a href="#" id="mDiminutivo" class="button round">Diminutivo</a>
+		<a href="#" id="mDyA" class="button round">Derivados y adjetivos</a>	
+		<a href="#" id="mSinonimos" class="button round">Sinonimos</a><br /> <br />
+		
+		<a href="<?=base_url()?>index.php/cDatosPerfil/PerfilUsuario/<?=$idJuego=1?>/<?=$idUsuario=2?>" id="bPerfil" class="large button round">Perfil</a>
+		<a id="bJuego" class="large button round">Juego</a>
+		 </center>
+		 
+		 	
 	</body>
 </html>
---
+++ media/js/modalidadJuego.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+   	$("#pRapida").attr("disabled", "disabled");
+   	$("#nBasico").attr("disabled", "disabled");
+   	$("#nIntermedio").attr("disabled", "disabled");
+   	$("#nAvanzado").attr("disabled", "disabled");
+   	$("#nExperto").attr("disabled", "disabled");
+   	$("#mJL").attr("disabled", "disabled");
+   	$("#mDiminutivo").attr("disabled", "disabled");
+   	$("#mDyA").attr("disabled", "disabled");
+   	$("#mSinonimos").attr("disabled", "disabled");
+   	$("#bJuego").attr("disabled", "disabled");
+
+    //Habilita las opciones posibles de la partida completa
+	$("#pCompleta").click(function() {
+   		$("#nBasico").removeAttr("disabled");
+   		$("#nAvanzado").removeAttr("disabled");
+		$("#vPartida").val(1)
+   		
+	});
+
+    //Habilita las opciones posibles de la partida básica
+	$("#nBasico").click(function() {
+   		$("#mJL").removeAttr("disabled");
+   		$("#mDiminutivo").attr("disabled", "disabled");
+	   	$("#mDyA").attr("disabled", "disabled");
+	   	$("#mSinonimos").attr("disabled", "disabled");
+	   	$("#bJuego").attr("disabled", "disabled");
+		$("#vNivel").val(1)
+	});
+	
+    //Habilita las opciones posibles de la partida rápida
+	$("#nAvanzado").click(function() {
+		$("#vNivel").val(3)
+   		$("#mJL").removeAttr("disabled");
+   		$("#mDiminutivo").removeAttr("disabled");
+   		$("#mSinonimos").removeAttr("disabled");
+   		$("#mDyA").removeAttr("disabled");
+   		$("#bJuego").attr("disabled", "disabled");
+	});
+	
+	$("#mJL").click(function() {
+		$("#vModalidad").val(1)
+		$("#bJuego").removeAttr("disabled");
+	});
+	
+	$("#mDiminutivo").click(function() {
+		$("#vModalidad").val(2)
+		$("#bJuego").removeAttr("disabled");
+	});
+	
+	$("#mDyA").click(function() {
+		$("#vModalidad").val(3)
+		$("#bJuego").removeAttr("disabled");
+	});
+	
+	$("#mSinonimos").click(function() {
+		$("#vModalidad").val(5)
+		$("#bJuego").removeAttr("disabled");
+	});
+	
+	$("#bJuego").click(function() {
+		var m=$("#vModalidad").val()
+		var n=$("#vNivel").val()
+		var p=$("#vPartida").val()
+		$("#bJuego").attr("href", base+"index.php/cJuego/configuracionJuego/"+p+"/"+n+"/"+m);
+	});
+	
+});
+
+
