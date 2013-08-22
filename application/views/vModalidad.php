aa5d0cd5517e824eaf496788ebaae2081fb4d437
anjudark89 <anjudark89@d24f31a6-42f3-10c3-1e4e-24255120a7fe>
2013-08-22 01:42:58 +0000

    21 agosto 2013 Nallely Flores Agregando CSS a la vista de modalidad

--- application/views/vModalidad.php
+++ application/views/vModalidad.php
@@ -7,6 +7,8 @@
 		<script>var base = "<?=base_url(); ?>"</script> 
 		<script src="<?=base_url(); ?>media/js/modalidadJuego.js" type="text/javascript" charset="utf-8"></script>
 		<link rel="stylesheet" href="<?=base_url(); ?>media/css/foundation.css">
+		<link rel="stylesheet" href="<?=base_url(); ?>media/css/general.css">
+		<link rel="stylesheet" href="<?=base_url(); ?>media/css/modalidadN.css">
 
 	</head>	
 	
@@ -14,27 +16,27 @@
 		<input type="hidden" id="vPartida" value="0" />
 		<input type="hidden" id="vNivel" value="0" />
 		<input type="hidden" id="vModalidad" value="0" />
-		
-		<h2>Principal</h2>
-		
-		<center> SELECCIONA UNO	 <br />
-		<a href="#" id="pCompleta" class="button round">PARTIDA COMPLETA</a>	
-		<a href="#" id="pRapida" class="button round">PARTIDA RÁPIDA</a> <br /> <br />
-		SELECCIONA UN NIVEL	 <br />
-		<a href="#" id="nBasico" class="button round">Básico</a>	
-		<a href="#" id="nIntermedio" class="button round">Intermedio</a>
-		<a href="#" id="nAvanzado" class="button round">Avanzado</a>
-		<a href="#" id="nExperto" class="button round">Experto</a> <br /> <br />
-		SELECCIONA UNA MODALIDAD	 <br />
-		<a href="#" id="mJL" class="button round">Juego Libre</a>	
-		<a href="#" id="mDiminutivo" class="button round">Diminutivo</a>
-		<a href="#" id="mDyA" class="button round">Derivados y adjetivos</a>	
-		<a href="#" id="mSinonimos" class="button round">Sinonimos</a><br /> <br />
-		
-		<a href="<?=base_url()?>index.php/cDatosPerfil/PerfilUsuario/<?=$idJuego=1?>/<?=$idUsuario=-1?>" id="bPerfil" class="large button round">Perfil</a>
-		<a id="bJuego" class="large button round">Juego</a>
-		 </center>
-		 
-		 	
+		<div class="three columns"></div>
+		<a href="<?=base_url()?>index.php/cDatosPerfil/PerfilUsuario/<?=$idJuego=1?>/<?=$idUsuario=-1?>" id="bPerfil" class="large button">Perfil</a>
+		<div id="modalidadCaja" class="eight columns"> 
+			<h3>Selecciona tipo de partida</h3>	 <br />
+			<center>
+			<a href="#" id="pCompleta" class="button round">PARTIDA COMPLETA</a>	
+			<a href="#" id="pRapida" class="button round">PARTIDA RÁPIDA</a> <br /> <br />
+			<h3>Selecciona nivel</h3><br />
+			<a href="#" id="nBasico" class="button round">Básico</a>	
+			<a href="#" id="nIntermedio" class="button round">Intermedio</a>
+			<a href="#" id="nAvanzado" class="button round">Avanzado</a>
+			<a href="#" id="nExperto" class="button round">Experto</a> <br /> <br />
+			<h3>Selecciona modalidad</h3> <br />
+			<a href="#" id="mJL" class="button round">Juego Libre</a>	
+			<a href="#" id="mDiminutivo" class="button round">Diminutivo</a>
+			<a href="#" id="mDyA" class="button round">Derivados y adjetivos</a>	
+			<a href="#" id="mSinonimos" class="button round">Sinonimos</a><br /> <br />
+			
+			<a id="bJuego" class="large button round">¡Jugar!</a>
+			</center>
+		 </div>
+		<div class="two columns"></div>
 	</body>
 </html>
